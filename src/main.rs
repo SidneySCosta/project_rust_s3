a3204f4271405bfa4918e526005c9314edc9537f
sidney.costa <sidneycosta156@gmail.com>
2023-10-13 15:15:18 -0300

    add upload with streming and parallelism

--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,102 @@
 use actix_cors::Cors;
 use actix_files as fs;
-use actix_web::{web, App, HttpResponse, HttpServer};
-use futures_util::stream::StreamExt;
+use actix_web::{web, App, Error, HttpResponse, HttpServer};
+use futures::future;
+use futures::stream::StreamExt;
 use rusoto_core::Region;
-use rusoto_s3::CreateMultipartUploadRequest;
 use rusoto_s3::{
-    CompleteMultipartUploadRequest, CompletedMultipartUpload, CompletedPart, S3Client,
-    UploadPartRequest, S3,
+    CompleteMultipartUploadRequest, CompletedMultipartUpload, CompletedPart,
+    CreateMultipartUploadRequest, S3Client, UploadPartRequest, S3,
 };
 use std::env;
-use std::time::Instant;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+// use tokio_multipart::Multipart;
+use log::{error, info, warn};
 use uuid::Uuid;
 
-// use std::fs::File;
-// use tokio::io::AsyncReadExt;
+const PART_SIZE: usize = 10 * 1024 * 1024 * 1024; // 10 GB
 
-async fn upload(mut payload: web::Payload) -> Result<HttpResponse, actix_web::Error> {
-    let mut bytes = web::BytesMut::new();
+async fn upload(mut payload: web::Payload) -> Result<HttpResponse, Error> {
+    info!("Iniciando o upload...");
 
-    while let Some(chunk) = payload.next().await {
-        bytes.extend_from_slice(&chunk?);
-    }
-
-    let s3_key = format!("uploads/{}", Uuid::new_v4()); // Gere uma chave única no S3 com UUID
-    let start = Instant::now(); // Começa a cronometrar
-
-    match upload_to_s3(&bytes, "rustarchive", &s3_key).await {
-        Ok(_) => {
-            let duration = start.elapsed();
-            Ok(HttpResponse::Ok().body(format!(
-                "Arquivo enviado com sucesso para o S3 no caminho: {}. Tempo de envio: {:?}",
-                s3_key, duration
-            )))
-        }
-        Err(e) => {
-            let duration = start.elapsed();
-            Ok(HttpResponse::InternalServerError().body(format!(
-                "Erro ao enviar para o S3: {}. Tempo de tentativa: {:?}",
-                e, duration
-            )))
-        }
-    }
-}
-async fn upload_to_s3(bytes: &web::BytesMut, bucket: &str, key: &str) -> Result<(), String> {
-    // Mudei o tipo de erro de retorno para String para simplificar
+    let s3_key = format!("uploads/{}", Uuid::new_v4());
     let s3_client = S3Client::new(Region::SaEast1);
+    info!("Usando o S3 Key: {}", s3_key);
 
-    // Inicie o upload multipart
     let create_req = CreateMultipartUploadRequest {
-        bucket: bucket.to_string(),
-        key: key.to_string(),
+        bucket: "rustarchive".to_string(),
+        key: s3_key.clone(),
         ..Default::default()
     };
+
     let create_resp = s3_client
         .create_multipart_upload(create_req)
         .await
-        .map_err(|e| format!("Erro ao criar upload multipart: {}", e))?;
+        .map_err(|e| {
+            error!("Erro ao iniciar o upload multipart: {}", e);
+            actix_web::error::ErrorInternalServerError(e)
+        })?;
+
     let upload_id = create_resp.upload_id.unwrap();
+    info!("Upload ID recebido: {}", upload_id);
 
-    // Suponhamos que cada parte tenha 5 MB
-    let part_size = 5 * 1024 * 1024;
-    let total_parts = (bytes.len() as f64 / part_size as f64).ceil() as usize;
+    let completed_parts: Arc<Mutex<Vec<CompletedPart>>> = Arc::new(Mutex::new(Vec::new()));
+    let mut upload_futures = Vec::new();
+    let mut buffer = Vec::new();
+    let mut part_number = 1;
 
-    let mut completed_parts: Vec<CompletedPart> = vec![];
+    info!("Processando os chunks...");
+    while let Some(chunk) = payload.next().await {
+        let chunk = chunk?;
+        buffer.extend_from_slice(&chunk);
 
-    for part_number in 1..=total_parts {
-        let start = (part_number - 1) * part_size;
-        let end = if part_number == total_parts {
-            bytes.len()
-        } else {
-            part_number * part_size
-        };
+        while buffer.len() >= PART_SIZE {
+            let upload_part = buffer.split_off(PART_SIZE);
 
-        let part_data = &bytes[start..end];
+            info!("Preparando para enviar parte número {}", part_number);
 
-        let upload_part_req = UploadPartRequest {
-            bucket: bucket.to_string(),
-            key: key.to_string(),
-            upload_id: upload_id.clone(),
-            body: Some(part_data.to_vec().into()),
-            part_number: part_number as i64,
-            ..Default::default()
-        };
+            let parts = completed_parts.clone();
+            let future = upload_single_part(
+                s3_client.clone(),
+                upload_part,
+                "rustarchive",
+                &s3_key,
+                &upload_id,
+                part_number,
+                parts,
+            );
 
-        let upload_part_resp = s3_client
-            .upload_part(upload_part_req)
-            .await
-            .map_err(|e| format!("Falha ao enviar parte {}: {}", part_number, e))?;
-
-        completed_parts.push(CompletedPart {
-            e_tag: upload_part_resp.e_tag,
-            part_number: Some(part_number as i64),
-        });
+            upload_futures.push(future);
+            part_number += 1;
+        }
     }
 
+    info!("Finalizando envio de chunks restantes...");
+    if !buffer.is_empty() {
+        let parts = completed_parts.clone();
+        let future = upload_single_part(
+            s3_client.clone(),
+            buffer,
+            "rustarchive",
+            &s3_key,
+            &upload_id,
+            part_number,
+            parts,
+        );
+
+        upload_futures.push(future);
+    }
+
+    info!("Aguardando todas as tarefas de upload concluírem...");
+    future::join_all(upload_futures).await;
+
     let complete_req = CompleteMultipartUploadRequest {
-        bucket: bucket.to_string(),
-        key: key.to_string(),
+        bucket: "rustarchive".to_string(),
+        key: s3_key.clone(),
         upload_id: upload_id,
         multipart_upload: Some(CompletedMultipartUpload {
-            parts: Some(completed_parts),
+            parts: Some(completed_parts.lock().await.clone()),
         }),
         ..Default::default()
     };
@@ -107,19 +104,66 @@ async fn upload_to_s3(bytes: &web::BytesMut, bucket: &str, key: &str) -> Result<
     s3_client
         .complete_multipart_upload(complete_req)
         .await
-        .map_err(|e| format!("Erro ao completar upload multipart: {}", e))?;
+        .map_err(|e| {
+            error!("Erro ao completar upload multipart: {}", e);
+            actix_web::error::ErrorInternalServerError(e)
+        })?;
+
+    info!(
+        "Upload concluído com sucesso para o S3 no caminho: {}",
+        s3_key
+    );
+    Ok(HttpResponse::Ok().body(format!(
+        "Arquivo enviado com sucesso para o S3 no caminho: {}",
+        s3_key
+    )))
+}
+
+async fn upload_single_part(
+    s3_client: S3Client,
+    part: Vec<u8>,
+    bucket: &str,
+    key: &str,
+    upload_id: &str,
+    part_number: i64,
+    completed_parts: Arc<Mutex<Vec<CompletedPart>>>,
+) -> Result<(), String> {
+    info!("Enviando a parte número {}", part_number);
+
+    let upload_part_req = UploadPartRequest {
+        bucket: bucket.to_string(),
+        key: key.to_string(),
+        upload_id: upload_id.to_string(),
+        body: Some(part.into()),
+        part_number,
+        ..Default::default()
+    };
+
+    let upload_part_resp = s3_client.upload_part(upload_part_req).await.map_err(|e| {
+        error!("Erro ao fazer upload da parte {}: {}", part_number, e);
+        format!("Erro ao fazer upload da parte {}: {}", part_number, e)
+    })?;
+
+    completed_parts.lock().await.push(CompletedPart {
+        e_tag: upload_part_resp.e_tag,
+        part_number: Some(part_number),
+    });
+
+    info!("Parte número {} enviada com sucesso", part_number);
 
     Ok(())
 }
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let bind_address = env::var("BIND_ADDRESS").unwrap_or_else(|_| "127.0.0.1:8088".to_string()); // porta padrão 8081
+    env_logger::init();
+    info!("Servidor inicializando...");
+
+    let bind_address = env::var("BIND_ADDRESS").unwrap_or_else(|_| "127.0.0.1:8088".to_string());
 
     HttpServer::new(|| {
         App::new()
-            .wrap(
-                Cors::permissive(), // permitindo cors para testes
-            )
+            .wrap(Cors::permissive()) // permitindo cors para testes
             .route("/upload", web::post().to(upload))
             .service(fs::Files::new("/download", "uploads/"))
     })
